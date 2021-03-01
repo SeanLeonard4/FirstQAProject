283b27624b65149a11405ff5090dd07d91ea64c6
Sean Leonard <Sleonard@qa.com>
2021-03-01 17:26:36 +0000

    Initialising repo

---
+++ src/main/java/com/qa/covidproject/FirstQaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.qa.covidproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstQaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstQaProjectApplication.class, args);
+	}
+
+}
